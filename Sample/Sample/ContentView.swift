736fe82acc39599ba9810685ca2c507985733937
sei13 <kamkamsirona13@gmail.com>
2021-01-09 12:26:49 +0900

    SampleProject Init Commit

---
+++ Sample/Sample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by 伊勢俊弥 on 2021/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, sei!")
+            .padding()
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample/Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by 伊勢俊弥 on 2021/01/09.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
