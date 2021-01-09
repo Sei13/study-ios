e35ea2fd63402d794c9679438a72d28b9406dcaa
sei13 <kamkamsirona13@gmail.com>
2021-01-09 12:36:12 +0900

    ImageView Ini committ

---
+++ Sample/Sample/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Sample
+//
+//  Created by 伊勢俊弥 on 2021/01/09.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
