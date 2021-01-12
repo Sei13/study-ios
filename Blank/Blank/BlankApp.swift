585214ecfa40381e875628783cb8061ce80b4c16
sei13 <kamkamsirona13@gmail.com>
2021-01-12 21:56:08 +0900

    Blank Project Commit

---
+++ Blank/Blank/BlankApp.swift
@@ -0,0 +1,21 @@
+//
+//  BlankApp.swift
+//  Blank
+//
+//  Created by 伊勢俊弥 on 2021/01/11.
+//
+
+import SwiftUI
+
+@main
+struct BlankApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+    
+    var int: Int = 10
+    var int_32: Int32 = 100
+    
+}
