47adbbea9b207e50768a82dbe8262916099cea89
sei13 <kamkamsirona13@gmail.com>
2021-01-13 23:46:48 +0900

    landmark Stract commit

--- Sample/Models/Landmark.swift
+++ Sample/Models/Landmark.swift
@@ -8,5 +8,23 @@
 import SwiftUI
 
 struct Landmark{
+    var id: Int
+    var name: String
+    var imageName: String
+    var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
     
+    enum Category: String, CaseIterable{
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+}
+
+struct Coordinates {
+    var latitude: Double
+    var longitude: Double
 }
